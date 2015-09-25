80f23afb66023d1af555bfb577ea7cc022167d26
Brian Peppard <BJPeppard@gmail.com>
2015-09-25 11:20:00 -0500

    exercise3

---
+++ exercise3Find.js
@@ -0,0 +1,15 @@
+var mongo = require('mongodb').MongoClient;
+
+var age = parseInt(process.argv[2]);
+//console.log('age: '+age);
+
+mongo.connect('mongodb://localhost:27017/learnyoumongo', function(err, db) {
+  var parrots = db.collection('parrots');
+  parrots.find({age: {$gt:age}}, function(err, cursor) {
+
+    cursor.toArray(function(err, documents) {
+      console.log(documents);
+      db.close();
+    });
+  });
+});
